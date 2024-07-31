bc5233562beb6c0f3ce30777b4f81a01429369a3
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-07-31 09:15:49 +0200

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
