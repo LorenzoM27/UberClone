8e887bdd604fb5284caf8f8dcfa9ea78a6ab5a66
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-08-18 10:20:39 +0200

    route generation, mapView states, search fort destination and change the map to it

---
+++ UberClone/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationSearchViewModel.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 15/08/2024.
+//
+
+import Foundation
---
+++ UberClone/Models/MapViewState.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewState.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 17/08/2024.
+//
+
+import Foundation
