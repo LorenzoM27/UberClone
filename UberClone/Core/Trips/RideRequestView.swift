e7c5cfa2d1b5198e5a82ea14c3eedef01a22d0cb
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-08-18 10:31:58 +0200

    riderequestview

---
+++ UberClone/Core/Trips/RideRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  RideRequestView.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 18/08/2024.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RideRequestView()
+}
