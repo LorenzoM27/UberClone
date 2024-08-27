437ba9e985293e2890cef1490c9d74eac240ee73
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-08-27 18:15:04 +0200

    ride request view

---
+++ UberClone/Extensions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 19/08/2024.
+//
+
+import Foundation
---
+++ UberClone/Models/UberLocation.swift
@@ -0,0 +1,8 @@
+//
+//  UberLocation.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 18/08/2024.
+//
+
+import Foundation
