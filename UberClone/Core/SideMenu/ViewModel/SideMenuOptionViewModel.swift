5eeaac964629b05896505db937788bc17a581aee
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-09-04 16:10:26 +0200

    Side menu

---
+++ UberClone/Core/SideMenu/ViewModel/SideMenuOptionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SideMenuOptionViewModel.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 03/09/2024.
+//
+
+import Foundation
---
+++ UberClone/Core/SideMenu/Views/SideMenuOptionView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuOptionView.swift
+//  UberClone
+//
+//  Created by Lorenzo Menino on 03/09/2024.
+//
+
+import SwiftUI
+
+struct SideMenuOptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuOptionView()
+}
